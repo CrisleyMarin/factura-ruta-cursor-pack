f9871656e5ce1b550bf107622c0d52ab816ac7c9
Crisley <Crisleymarin00@gmail.com>
2026-06-03 21:12:49 -0500

    Refactor code structure for improved readability and maintainability

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "factura-ruta-v4-no-zero-row";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.jpg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
